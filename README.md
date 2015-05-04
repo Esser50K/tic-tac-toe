0063471fa71f99593e24c962e25c48bf2bff1c6c
Spencer Cornelia <cornelia.spencer@yahoo.com>
2015-05-04 08:45:01 -0700

    project1

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Today we will be making a Tic Tac Toe game using all of this.
 * A user should be able to click on different squares to make a move.
 * Every click will alternate between marking an `X` and `O`
 * Upon marking of an individual cell, use javascript to add a class to each cell to display separate colors.
-* A cell should not be able able to replayed once marked.
+* A cell should not be able to be replayed once marked.
 * Add a reset button that will clear the contents of the board.
 
 ##How to get started
--- app.js
+++ app.js
@@ -1,6 +1,90 @@
-// wait for the DOM to finish loading
-window.addEventListener(function () {
-  // all your code should go in here.
+window.addEventListener("load", function () {	
+	var eachSquare = document.querySelectorAll(".cell");
+	for (i=0; i < eachSquare.length; i++) {
+		eachSquare[i].addEventListener("click", newMark);
+	}
+
+var turn = "X";
+function newMark(click) {
+	if (turn === "X") {
+		this.innerHTML = "X";
+		this.style.color = "purple";
+		turn = "O";
+		alert("Your turn O");
+	}
+	else {
+		this.innerHTML = "O";
+		this.style.color = "orange";
+		turn = "X";
+		alert("Your turn X");
+	}
+}
 
 
-});
+document.getElementById("Button").addEventListener("click", clearBoard);
+function clearBoard() {
+	var newBoard = document.querySelectorAll('#board');
+	for (i=0; i < newBoard.length; i++) {
+		newBoard[i].style.width = '31em';
+		newBoard[i].style.height = '30em';
+		newBoard[i].style.position = 'relative';
+		newBoard[i].style.top = '30px';
+		newBoard[i].style.left = '10%';
+		newBoard[i].style.color = "aqua";
+	var topleft = document.querySelector("#topleft");
+	topleft.innerHTML = "1";
+	var topCenter = document.querySelector("#topcenter");
+	topCenter.innerHTML = "2";	
+	var topRight = document.querySelector("#topright");
+	topRight.innerHTML = "3";
+	var midLeft = document.querySelector("#midleft");
+	midLeft.innerHTML = "4";
+	var midCenter = document.querySelector("#midcenter");
+	midCenter.innerHTML = "5";
+	var midRight = document.querySelector("#midright");
+	midRight.innerHTML = "6";
+	var bottomLeft = document.querySelector("#bottomleft");
+	bottomLeft.innerHTML = "7";
+	var bottomCenter = document.querySelector("#bottomcenter");
+	bottomCenter.innerHTML = "8";
+	var bottomRight = document.querySelector("#bottomright");
+	bottomRight.innerHTML = "9";
+		}		
+	}
+// var random1 = document.querySelector("#topleft");
+// var random2 = document.querySelector("#topcenter");
+// var random3 = document.querySelector("#topright");
+// var random4 = document.querySelector("#midleft");
+// var random5 = document.querySelector("#midcenter");
+// var random6 = document.querySelector("#midright");
+// var random7 = document.querySelector("#bottomleft");
+// var random8 = document.querySelector("#bottomcenter");
+// var random9 = document.querySelector("#bottomright");
+
+// if (random1.textContent === "X" && random2.textContent === "X" && random3.textContent === "X") {
+// 	alert("X is the winner!");
+// }
+// else if (random4.textContent === "X" && random5.textContent === "X" && random6.textContent === "X") {
+// 	alert("X is the winner!");
+// }
+// else if (random7.textContent === "X" && random8.textContent === "X" && random9.textContent === "X") {
+// 	alert("X is the winner!");
+// }
+// else if (random1.textContent === "O" && random2.textContent === "O" && random3.textContent === "O") {
+// 	alert("O is the winner!");
+// }
+// else if (random7.textContent === "X" && random8.textContent === "X" && random9.textContent === "X") {
+// 	alert("O is the winner!");
+// }
+
+});	
+
+
+// var one = document.getElementById("#topleft");
+// var two = document.getElementById("topcenter");
+// var three = document.getElementById("topright");
+// var winner = function () {
+// 	if (one.textContent === "X" && two.textContent === "X" && three.textContent === "X") {
+// 		alert("X wins");
+// 	}
+// }	
